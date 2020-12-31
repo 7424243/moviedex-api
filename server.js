17776da66e5158ba020c2b9c5857e5f820469040
Sam Sheets <sam.sheets94@gmail.com>
2020-12-31 13:54:07 -0700

    implemented security measures with cors and helmet

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 const MOVIE = require('./movie-data.json')
 
 console.log(process.env.API_TOKEN)
@@ -8,6 +10,8 @@ console.log(process.env.API_TOKEN)
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization')
