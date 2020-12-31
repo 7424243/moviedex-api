f69adebe4fe9792a814d5745956c406c60dc003a
Sam Sheets <sam.sheets94@gmail.com>
2020-12-31 13:39:38 -0700

    created data file and implemented handleGetMovie function for /movie

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const morgan = require('morgan')
+const MOVIE = require('./movie-data.json')
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-    res.send('Hello, world!')
-})
+function handleGetMovie(req, res) {
+    let response = MOVIE
+    const {genre, country, avg_vote} = req.query
+    if(genre) {
+        response = response.filter(movie => 
+            movie.genre.toLowerCase().includes(genre.toLowerCase()))
+    }
+    if(country) {
+        response = response.filter(movie => 
+            movie.country.toLowerCase().includes(country.toLowerCase()))
+    }
+    if(avg_vote) {
+        response = response.filter(movie =>
+            movie.avg_vote >= avg_vote)
+    }
+    res.json(response)
+}
+
+app.get('/movie', handleGetMovie)
 
 app.listen(8000, () => {
     console.log('Server listening at http://localhost:8000')
