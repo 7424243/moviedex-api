5f853a40b9c2107294d4064abfd7f2bbf0688fa7
Sam Sheets <sam.sheets94@gmail.com>
2021-01-01 09:25:31 -0700

    completed prepare for deployment steps

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const MOVIES = require('./movie-data.json')
 
 const app = express()
 
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors())
@@ -42,6 +43,18 @@ function handleGetMovie(req, res) {
 
 app.get('/movie', handleGetMovie)
 
-app.listen(8000, () => {
+app.use((error, req, res, next) => {
+    let response
+    if(process.env.NODE_ENV === 'production') {
+        reponse = { error: { message: 'server error'}}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
     console.log('Server listening at http://localhost:8000')
 })
